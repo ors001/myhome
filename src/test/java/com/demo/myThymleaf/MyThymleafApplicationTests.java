d8391ae26786a88342b14f025859829d10f628c1
ors001 <ors001@naver.com>
2021-02-21 13:05:29 +0900

    Init Project

---
+++ src/test/java/com/demo/myThymleaf/MyThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.myThymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyThymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
